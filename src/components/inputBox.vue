3e0225bb631006a7cf5cc2cd69979720a74eec93
YukiAoi <xxz530990200@gmail.com>
2025-02-21 15:44:16 +0800

    输入框

--- src/components/inputBox.vue
+++ src/components/inputBox.vue
@@ -9,20 +9,45 @@
     ></el-input>
     <div class="other-box">
       <div>
-        <el-button
-          @click="methods.changeStatus('isDeep')"
-          :type="data.isDeep ? 'primary' : ''"
+        <el-tooltip
+          effect="dark"
+          content="调用新模型R1，解决推理问题"
+          placement="left"
         >
-          深度思考
-        </el-button>
-        <el-button
-          @click="methods.changeStatus('isOnline')"
-          :type="data.isOnline ? 'primary' : ''"
+          <el-button
+            @click="methods.changeStatus('isDeep')"
+            :type="data.isDeep ? 'primary' : ''"
+            icon="Opportunity"
+          >
+            深度思考
+          </el-button>
+        </el-tooltip>
+        <el-tooltip effect="dark" content="按需搜索网页" placement="right">
+          <el-button
+            @click="methods.changeStatus('isOnline')"
+            :type="data.isOnline ? 'primary' : ''"
+            icon="Search"
+          >
+            联网搜索
+          </el-button>
+        </el-tooltip>
+      </div>
+      <div>
+        <el-tooltip
+          effect="dark"
+          :content="
+            data.isOnline ? '联网搜索不支持上传文件' : '上传附件（仅识别文字）'
+          "
+          placement="top"
         >
-          联网搜索
+          <el-button circle :disabled="data.isOnline">
+            <el-icon :size="20"><Paperclip /></el-icon>
+          </el-button>
+        </el-tooltip>
+        <el-button type="primary" circle :disabled="data.content === ''">
+          <el-icon :size="20"><Top /></el-icon>
         </el-button>
       </div>
-      <div></div>
     </div>
   </div>
 </template>
